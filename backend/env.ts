030aca50dab07035220d09a16c9de430e8b62781
amarnarute <amarnarute2002@gmail.com>
2021-02-24 23:32:33 +0530

    env Variables

--- backend/env.ts
+++
@@ -1,3 +0,0 @@
-export enum env {
-  MONGO_URI = 'mongodb+srv://NewAmar:OfuAR4iKLMjZsQan@newamar.9xxre.mongodb.net/UrlShort?authSource=admin&replicaSet=atlas-o5fwhb-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',
-}
