eba13b182d1d421ed51328c29c23f8b3a19a5dc2
amarnarute <amarnarute2002@gmail.com>
2021-07-21 11:44:56 +0530

    env

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { urlRouter } from './routes/url';
 import mongoose from 'mongoose';
-import { env } from './env/env';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(urlRouter);
 
-mongoose.connect(env.MONGO_URI, {
+mongoose.connect(`${process.env.MONGO_URI}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
