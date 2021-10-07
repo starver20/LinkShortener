5e9a63f2985eb4a07dc8f0f182187b88eb10affb
starver20 <amarnarute2002@gmail.com>
2021-10-07 19:31:05 +0530

    Cors

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { urlRouter } from './routes/url';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
-
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(urlRouter);
 
